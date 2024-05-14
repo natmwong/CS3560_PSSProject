d79bc855531677f8e0a2edd84a18af485a727c44
Natasha <wong.natasha.m@gmail.com>
2024-05-14 14:05:13 -0700

    added add task method to controller

--- controller.py
+++ controller.py
@@ -1,7 +1,20 @@
+from task import Task
+from recurringTask import RecurringTask
+from transientTask import TransientTask
+from antiTask import AntiTask
+
 class Controller:
     def __init__(self, model):
         self.model = model
 
-    def handle_user_input(self):
-        # Handle user input and interact with model/viewer
-        pass
+    # Add a task to the model
+    def add_task(self, start_date, end_date, task_description, task_type, recurrence_pattern):
+        if task_type == "Transient Task":
+            task = TransientTask(start_date, end_date, task_description)
+        elif task_type == "Recurring Task":
+            task = RecurringTask(start_date, end_date, task_description, recurrence_pattern)
+        else:
+            task = AntiTask(start_date, end_date, task_description)
+        self.model.add_task(task)
+
+        
